d00af6e3fe06dc2c23c1d7ca65ba50061fd29c20
Michael Berner <michael.berner@dmc.de>
2017-12-15 12:37:42 +0100

    Added some sample option for sshfs add-server and print mountfoldername to connect list

--- sshfs-manager.sh
+++ sshfs-manager.sh
@@ -37,7 +37,7 @@ showMessage() {
             [ADD_SERVER_USER]='\t SSH username: '
             [ADD_SERVER_SOURCEDIR]='\t Server source directory to be mounted (eg: public_html): '
             [ADD_SERVER_MOUNTDIR]='\t Local mount directory under global mount path (eg: mydomain_com): '
-            [ADD_SERVER_SSHFSOPTIONS]='\t SSHFS options (any option provided by sshfs --help): '
+            [ADD_SERVER_SSHFSOPTIONS]='\t SSHFS options (any option provided by sshfs --help) \n\t Sample option: \n\t\t-o auto_unmount,transform_symlinks,follow_symlinks,ServerAliveInterval=30,IdentityFile=~/.ssh/id_rsa,idmap=user,uid=1000,gid=1000\n\t Options: '
             [ADD_SERVER_SUCCESS]="[SSHFS-MGR] Server has been successfully saved.\n"
             [ADD_SERVER_SUCCESS_POST]='[SSHFS-MGR] You can now connect to %s using the "connect" command.\n'
             [LOAD_DATA_FILE_ERROR]='[SSHFS-MGR] No installation found. Please run the "install" command.\n'
@@ -263,7 +263,10 @@ connectPrompt() {
     # Print available server list
     showMessage AVAILABLE_SERVERS
     for index in ${!serverList[*]}; do
-        printf "\t$index - ${serverList[$index]}\n"
+        serverDomain=${serverList[$index]}
+        mountDir=${servers[$serverDomain,'mountDir']}
+        domain=${serverList[$index]}
+        printf "\t$index - $mountDir @ $domain \n"
     done
 
     # Let user choose a server from the list
